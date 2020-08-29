6366c9951937cb04a172326c85155b8ae86d2c27
KevinDeBroe <56866036+KevinDeBroe@users.noreply.github.com>
2020-08-29 17:46:06 +0200

    29/08/2020

---
+++ ConsoleMethodes/Methodes Basic/Basic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methodes_Basic
+{
+    class Basic
+    {
+        public static void Main(string[] args)
+        {
+            int result = Add(1, 8);
+            if (result > 10)
+            {
+                Console.WriteLine("Result is lazger than 10!");
+            }
+            else
+            {
+                Console.WriteLine("Result is smaller than or equal to 10!");
+            }
+            Console.ReadKey();
+
+        }
+
+        public static int Add (int num01, int num02)
+        {
+            return num01 + num02;
+        }
+        
+                
+    }
+}
